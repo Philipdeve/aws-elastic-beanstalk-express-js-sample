256aa09ecb05f75d9f056f9bfef4f17be4033606
PHILIPS IFEANYI <pio14philip@gmail.com>
2023-07-15 13:28:47 +0100

    feat: Final pipeline test

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Full Pipeline Test'));
+app.get('/', (req, res) => res.send('Final Pipeline Test'));
 app.get('/login', (req, res) => res.send('Provide username and password'));
+app.get('/signup', (req, res) => res.send('Provide username and password'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
